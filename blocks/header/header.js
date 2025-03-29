db1e544033a9f4befdc0d0163d9097634a2dc4dc
himai <himai@adobe.com>
2025-03-30 00:43:15 +0900

    header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -164,17 +164,22 @@ if (brandLink) {
 // nav-sections に nav-drop を設定し、クリックで開閉
 const navSections = nav.querySelector('.nav-sections');
 if (navSections) {
-  navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
-    if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-    navSection.addEventListener('click', () => {
-      if (isDesktop.matches) {
-        const expanded = navSection.getAttribute('aria-expanded') === 'true';
-        toggleAllNavSections(navSections);
-        navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  navSections
+    .querySelectorAll(':scope .default-content-wrapper > ul > li')
+    .forEach((navSection) => {
+      if (navSection.querySelector('ul')) {
+        navSection.classList.add('nav-drop');
       }
+      navSection.addEventListener('click', () => {
+        if (isDesktop.matches) {
+          const expanded = navSection.getAttribute('aria-expanded') === 'true';
+          toggleAllNavSections(navSections);
+          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        }
+      });
     });
-  });
 }
+
   // hamburger for mobile
   const hamburger = document.createElement('div');
   hamburger.classList.add('nav-hamburger');
